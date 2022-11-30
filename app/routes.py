b1b4fc98b8b4b3bd39d99cca32d5b291e3245cdf
Esteban Puma <estepuma94@gmail.com>
2022-11-29 22:48:08 -0500

    navbar y footer primeros pasos con css

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app, db
-from flask import render_template, redirect, url_for, flash
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, WeekData
 from app.forms import LoginForm, SignupForm, WeekDataForm
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
@@ -25,7 +26,10 @@ def login():
             flash('Password incorrecto')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', form=form)
 
 
@@ -44,7 +48,18 @@ def signup():
     return render_template('signup.html', form=form)
 
 
+@app.route('/profile/<email>')
+@login_required
+def profile(email):
+    user = User.query.filter_by(email=email).first_or_404()
+    week_data = [
+        {'user': user, 'weight': 83},
+        {'user': user, 'weight': 84}
+    ]
+    return render_template('profile.html', user=user, week_data=week_data)
+
 @app.route('/week_data')
+@login_required
 def week_data():
     return render_template('week_data.html')
 
